f0243e2442439ca979cd25b5c88729ea4e4fb9fe
Franco Valdez <franco.martin.valdez@mi.unc.edu.ar>
2023-10-29 11:13:26 -0300

    sol cookie

--- server/src/controllers/user.controllers.js
+++ server/src/controllers/user.controllers.js
@@ -45,7 +45,7 @@ const Login = async(req,res)=>{
             }
             //generamos token !!!
         const token=jwt.sign({id:findUser._id,username:findUser.username,email:findUser.email},SECRET_KEY_JWTOKEN,{expiresIn:"1d"})
-        res.cookie('token', token,{sameSite:"None",domain:CLIENT_URL}).json({id:findUser._id,username:findUser.username,email:findUser.email,token})
+        res.cookie('token', token,{sameSite:"Lax"}).json({id:findUser._id,username:findUser.username,email:findUser.email,token})
     } catch (error) {
         console.log(error.message);
         res.status(400).json(error)
